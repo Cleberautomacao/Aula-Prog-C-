5ba3b1d62014427c2ea12fc76b1f48658c8a8de1
Cleberautomacao <48035261+Cleberautomacao@users.noreply.github.com>
2019-02-26 21:52:52 -0300

    Add files via upload

---
+++ A maior que B.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+	int A=0;
+	int B=0;
+	
+	printf("informe um numero inteiro A:\n");
+	
+	scanf("%i",&A);
+	
+	printf("informe um numero inteiro B:\n");
+	
+	scanf("%i",&B);
+	printf("\n\n");
+	
+	if (A>B)
+	
+	
+	{
+		printf ("Ae maior que B!\n");
+	}
+		
+		
+		else
+		{
+			if(A<B)
+		
+		{
+		printf("B e maior que A!\n");
+	}
+		
+			else 
+		{
+		
+
+			printf("B e = A!\n)");
+		}
+			return 1;
+		}
+   }
